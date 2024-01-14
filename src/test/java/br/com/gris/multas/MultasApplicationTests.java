cd92c4796321b4ebd866df2e0fcb4631feca9954
Rafael-Alexandre-96 <rafael.alexandre.1996.ra@gmail.com>
2024-01-13 23:36:31 -0300

    Iniciando Projeto

---
+++ src/test/java/br/com/gris/multas/MultasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gris.multas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
